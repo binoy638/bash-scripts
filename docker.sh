7a148eb73beb0ca7937e120efac8f7921604e1ab
Binoy Barman <55360337+binoy638@users.noreply.github.com>
2022-03-08 14:48:53 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -37,6 +37,8 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
+sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge test"
+
 apt-cache policy docker-ce
 
 sudo apt install docker-ce
